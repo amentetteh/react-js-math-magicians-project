026b47d5c3aa6478dad83a66b9ed349e106fb0e1
Amen TETTEH <tkamen@outlook.com>
2022-12-14 22:35:20 +0000

    Add Home.js file for the the page

---
+++ src/components/Home.js
@@ -0,0 +1,37 @@
+function Home() {
+  return (
+    <div className="home">
+      <h3>Welcome to our Page</h3>
+
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut non quam a risus
+        eleifend tincidunt. Maecenas rhoncus porta ex, at sodales tortor consequat
+        vitae. Aliquam fringilla lacus elit, vel eleifend lorem sodales eget.
+        Sed a euismod mi, eget lacinia dolor. Nulla egestas lorem ut
+        ligula auctor, et fermentum nibh commodo. Pellentesque ut purus accumsan,
+        aliquam ligula quis, dictum erat. Ut vitae dignissim quam.
+        Curabitur nulla enim, interdum vel nibh volutpat, porttitor
+        viverra turpis. Donec eu libero sit amet sapien ornare aliquet sed vel erat.
+        Donec ultricies, leo sed ullamcorper mollis, est erat
+        pharetra augue, eget consequat ipsum lacus in sapien.
+        In bibendum et sapien a facilisis. Quisque lacinia mi sit
+        amet finibus eleifend. Maecenas sit amet rhoncus mauris.
+        Suspendisse tempor lacus non sapien venenatis dapibus.
+        Sed placerat ac velit non suscipit. Cras id maximus libero.
+      </p>
+      <p>
+        Pellentesque ullamcorper tempus neque in vestibulum. Pellentesque
+        sit amet urna erat. Vestibulum pellentesque lectus id ipsum
+        semper, a venenatis erat feugiat. Aliquam porta iaculis ligula
+        id mollis. Aenean id pellentesque risus. Morbi lobortis fermentum
+        rhoncus. Etiam efficitur vitae eros ac dictum.
+        Integer id erat sit amet neque maximus hendrerit. Maecenas quis massa nec leo tincidunt
+        convallis. In consequat aliquet neque, nec semper metus vehicula nec. Donec tincidunt
+        ligula nec fermentum feugiat. Orci varius natoque penatibus et magnis dis parturient
+        montes, nascetur ridiculus mus.
+      </p>
+    </div>
+  );
+}
+
+export default Home;
