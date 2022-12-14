4589a766c7e45794bb0711b7d9723bb7d08abb5b
Amen TETTEH <tkamen@outlook.com>
2022-12-14 23:29:04 +0000

    Update App.js file for routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import Calculator from './components/Calculator';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import CalculatorPage from './components/CalculatorPage';
+import Header from './components/Header';
+import Quote from './components/Quote';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Header
+        <Header />
       </header>
-      <main className="content">
-        <Calculator />
-      </main>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/calculator" element={<CalculatorPage />} />
+        <Route path="/quote" element={<Quote />} />
+      </Routes>
     </div>
   );
 }
