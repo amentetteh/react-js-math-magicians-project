6583bd93487b33416bc42aa68e53a526940aa30e
enoqueJonas <enoque.macanda@gmail.com>
2022-12-16 11:58:47 +0200

    UPDATE components.test.js tests

--- src/tests/components.test.js
+++ src/tests/components.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-// import { BrowserRouter as Router } from 'react-router-dom';
-// import Header from '../components/Header';
-// import Quote from '../components/Quote';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Header from '../components/Header';
+import Quote from '../components/Quote';
 import Home from '../components/Home';
 import CalculatorPage from '../components/calculator-page/CalculatorPage';
 
@@ -13,9 +13,21 @@ describe('Test for operation', () => {
       .toJSON();
     expect(tree).toMatchSnapshot();
   });
-  it('CalculatorPage component should render correctly', () => {
+  it('Calculator Page component should render correctly', () => {
     const tree = renderer
       .create(<CalculatorPage />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+  it('Header component should render correctly', () => {
+    const tree = renderer
+      .create(<Router><Header /></Router>)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+  it('Quote component should render correctly', () => {
+    const tree = renderer
+      .create(<Quote />)
       .toJSON();
     expect(tree).toMatchSnapshot();
   });
